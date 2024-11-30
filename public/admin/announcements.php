0ab75861280abb25ae9872c8d8e8a07d2ba02786
MengZe2 <i@mengze2.cn>
2024-11-30 19:19:27 +0800

    Update announcements.php

--- public/admin/announcements.php
+++ public/admin/announcements.php
@@ -31,7 +31,6 @@ $announcements = $db->query('SELECT * FROM announcements ORDER BY created_at DES
     <title>后台管理 - 公告管理</title>
     <link href="https://cdn.bootcdn.net/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" rel="stylesheet">
     <link href="https://cdn.bootcdn.net/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
-    <?php include('style.php'); ?>
 </head>
 <body class="bg-gradient-to-r from-blue-50 via-purple-50 to-pink-50">
     <?php include('header.php'); ?>
