c55e0bffd61a7ffbb9c32da4eb5f30218cc693a9
MengZe2 <i@mengze2.cn>
2024-11-30 08:39:51 +0800

    更新 index.php

--- public/admin/index.php
+++ public/admin/index.php
@@ -7,7 +7,6 @@ $config = require '../../config.php';
 $db = new Database($config);
 $auth = new Auth($db);
 
-// 检查是否已登录
 if ($auth->isAuthenticated()) {
     header('Location: dashboard.php');
     exit;
